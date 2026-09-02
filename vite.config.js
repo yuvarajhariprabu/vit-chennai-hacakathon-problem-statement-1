ec11de3edcb4b5953e145a3932843c6ab3c22e9f
yuvaraj <yuvaraj060108@gmail.com>
2026-09-02 15:25:14 +0530

    Add CombinedExperience.html and single-file build config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
 })
